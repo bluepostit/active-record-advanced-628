2a026ed2b5ebc4bb6948e1f1ab4b29a872c6858c
Yair <32864499+bluepostit@users.noreply.github.com>
2021-05-18 11:56:30 +0300

    validations, schema diagram

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,8 @@ class Doctor < ActiveRecord::Base
   # has_many(:patients, {through: :consultations})
   has_many :consultations
   has_many :patients, through: :consultations
+
+  # validates :last_name, uniqueness: true
+  validates :last_name, uniqueness: { scope: :first_name }
+  validates :first_name, presence: true
 end
